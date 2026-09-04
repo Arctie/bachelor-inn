8cd22cbf22524097c540e8b8ec13188261a6d339
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-09-04 09:31:56 +0200

    Added a mission context file to Core, for mission objectives

--- scripts/AI/BehaviourTree/bt_blackboard.gd
+++ scripts/AI/BehaviourTree/bt_blackboard.gd
@@ -3,6 +3,13 @@ class_name BTBlackboard
 
 var unit: Character
 var state: GameState
-#var context: MissingContext # For objectives
+var context: MissionContext # For objectives
 var chosen_command: Command = null
 var target: Character = null
+var target_pos: Vector3i = Vector3i.ZERO
+
+func _init(in_unit: Character, in_state: GameState, in_context: MissionContext) -> void:
+	unit = in_unit
+	state = in_state
+	context = in_context
+	#pass
---
+++ scripts/Core/mission_context.gd
@@ -0,0 +1,8 @@
+extends RefCounted
+class_name MissionContext
+
+var prioriy_targets: Array[Character] = []
+var prioriy_tiles: Array[Vector3i] = []
+var defend_tiles: Array[Vector3i] = []
+var current_round: int = 0
+var max_rounds: int = 0
