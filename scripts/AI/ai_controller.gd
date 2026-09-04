141c04faed50d1e3049bcdd731766797d78047d8
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-09-04 11:10:31 +0200

    AIController structure for chosing to run minimax or BT

--- scripts/AI/ai_controller.gd
+++ scripts/AI/ai_controller.gd
@@ -1,6 +1,29 @@
 extends RefCounted
 class_name AIController
+## This checks if the enemy should run minimax or bt to create a move.
 
-# func pick_a_move()
+static func choose_move(enemy: Character, state: GameState, context: MissionContext) -> Command:
+	match enemy.state.ai_mode:
+		CharacterState.AIMode.BEHAVIOUR_TREE:
+			return _run_bt(enemy, state, context)
+		CharacterState.AIMode.MINIMAX:
+			return _run_minimax(enemy, state) ## context not added to minimax yet
+		_:
+			return Wait.new(enemy.state.grid_position)
+
+static func _run_bt(enemy: Character, state: GameState, context: MissionContext) -> Command:
+	var cmd := BTRunner.run(enemy, state, context)
+	if cmd == null:
+		print("Running BT failed. Initiating Wait at Position.")
+		return Wait.new(enemy.state.grid_position)
+	return cmd
+
+static func _run_minimax(enemy: Character, state: GameState) -> Command:
+	var ai := MinimaxAI.new()
+	var cmd := ai.choose_best_move(state, enemy.state.search_depth, enemy)
+	if cmd == null:
+		print("Running MiniMax failed. Initiating Wait at Position.")
+		return Wait.new(enemy.state.grid_position)
+	return cmd
 # func run_minimax()
 # func run_bt()
