a03fef672325aeee93c79f068edacc787498728a
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-09-04 09:26:54 +0200

    BTNode as baseclass

--- scripts/AI/BehaviourTree/bt_node.gd
+++ scripts/AI/BehaviourTree/bt_node.gd
@@ -1,8 +1,9 @@
-extends Node
-# class_name BTNode
-#
-#enum Status { SUCCESS, FAILURE, RUNNING }
-#
-## Evaluates the node's logic. Returns a Status.
-#func tick(actor: Node, blackboard: Dictionary) -> Status:
+extends RefCounted
+class_name BTNode
+
+enum Status { SUCCESS, FAILURE, RUNNING }
+
+# Evaluates the node's logic. Returns one out of three Statuses.
+func tick(blackboard: Dictionary) -> Status:
 	#return Status.SUCCESS
+	return Status.FAILURE
